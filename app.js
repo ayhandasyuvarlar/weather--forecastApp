a0e4ef5db104d2b9c5584968bcbd848805576920
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-08-11 15:05:38 +0300

    added dizayn

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-let  imageName = document.getElementById('imageName')
-let  desc = document.getElementById('desc')
-
-function updateImg(){
-    if(desc.innerText === 'r'){
-        imageName.src = 'https://yastatic.net/weather/i/icons/funky/dark/ovc_ra.svg'
-    }
+let imageName = document.getElementById("imageName");
+let desc = document.getElementById("desc");
+let searchIcon = document.getElementById("searchIcon");
+let closeIcon = document.getElementById("closeIcon");
+let searchContent = document.getElementById("searchContent");
+function updateImg() {
+  if (desc.innerText === "r") {
+    imageName.src =
+      "https://yastatic.net/weather/i/icons/funky/dark/ovc_ra.svg";
+  }
 }
-updateImg()
+updateImg();
+function addEventList() {
+  searchIcon.addEventListener("click", getSearchBox);
+}
+
+function getSearchBox() {
+    searchContent.style.width = '100%'
+    searchContent.style.transition ='1s'
+}
+
+getSearchBox()
