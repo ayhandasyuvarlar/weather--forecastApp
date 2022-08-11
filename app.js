236a224850988332b422994ca971a09bf7346ad2
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-08-11 13:57:41 +0300

    html

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+let  imageName = document.getElementById('imageName')
+let  desc = document.getElementById('desc')
 
+function updateImg(){
+    if(desc.innerText === 'r'){
+        imageName.src = 'https://yastatic.net/weather/i/icons/funky/dark/ovc_ra.svg'
+    }
+}
+updateImg()
