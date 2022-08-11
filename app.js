335adfbc32c688a838fed67674e1b2e12e6c3539
Ayhan Dasyuvarlar <ayhandasyuvarlar@gmail.com>
2022-08-11 17:17:18 +0300

    fetch process

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ let desc = document.getElementById("desc");
 let searchIcon = document.getElementById("searchIcon");
 let closeIcon = document.getElementById("closeIcon");
 let searchContent = document.getElementById("searchContent");
+const url = "https://api.openweathermap.org/data/2.5/";
+const key = "45c9fd503cc5ddd3023a83eea837cb25";
+const searchBar = document.getElementById("searchBar");
+
+const setQuery = (e) => {
+  if (e.keyCode == "13") {
+    getResult(searchBar.value);
+  }
+};
+const getResult = (cityName) => {
+  let query = `${url}weather?q=${cityName}&appid=${key}&units=metric`;
+  fetch(query)
+  .then(response => {
+    return response.json()
+  })
+  .then(displayResult)
+};
+
+const displayResult = (result) => {
+    console.log(result)
+    let city = document.querySelector('.cityName')
+    let temp = document.querySelector('.temp')
+    city.innerText = `${result.name}, ${result.sys.country}`
+    temp.innerHTML = `${result.main.temp} °C` 
+}
 function updateImg() {
   if (desc.innerText === "r") {
     imageName.src =
@@ -10,9 +35,11 @@ function updateImg() {
   }
 }
 updateImg();
+
 function addEventList() {
   searchIcon.addEventListener("click", getSearchBox);
   closeIcon.addEventListener("click", deleteSearchBox);
+  searchBar.addEventListener("keypress", setQuery);
 }
 addEventList();
 
@@ -22,5 +49,5 @@ function getSearchBox() {
   searchContent.style.float = "left";
 }
 function deleteSearchBox() {
-  searchContent.style.width = "0%";
+  searchContent.style.width = "0px";
 }
